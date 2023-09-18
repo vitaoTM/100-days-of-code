65e95ad50955cfc21afc1311211070d21e2cf8e6
vitor <vitor.grosskopf@gmail.com>
2023-09-18 09:04:03 -0300

    Adding methodsto mastermind proj

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -5,5 +5,41 @@ class Code
     "B" => :blue,
     "Y" => :yellow
   }
+  attr_reader :pegs
 
+  def self.valid_pegs?(chars)
+    chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase) }
+  end
+
+  def self.random(length)
+    random_pegs = []
+    length.times { random_pegs << POSSIBLE_PEGS.keys.sample }
+    Code.new(random_pegs)
+  end
+
+  def self.from_string(string)
+    Code.new(string.split(""))
+
+  end
+
+  def initialize(chars)
+    # Code.valid_pegs?(chars)
+    if Code.valid_pegs?(chars)
+      @pegs = chars.each(&:upcase!)
+    else
+      raise "noo"
+    end
+  end
+
+  def [](index)
+    @pegs.[](index)
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(Code.new())
+    s
+  end
 end
--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,8 @@
 require_relative "code"
 
 class Mastermind
+  # def initialize(length)
+  #   @length = length
+  # end
 
 end
