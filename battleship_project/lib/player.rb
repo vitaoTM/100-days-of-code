5898a8da736c55511522f7fcc3e704ebbb50b0ef
vitaoTM <vitor.grosskopf@gmail.com>
2023-09-20 01:57:18 -0300

    Finished BattleShip console/terminal game

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,12 @@
 class Player
+  def get_move
+    puts "enter a position coordinates separated with a space like '4 7': "
+    arr = gets.chomp
+    guess = []
+    arr.split(' ') do |char|
+      guess << char.to_i
+    end
+  guess
+  end
 
 end
