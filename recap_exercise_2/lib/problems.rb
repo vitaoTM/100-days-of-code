12c0cff1ee1de6ec829d73fdfd91329680502a02
vitor <vitor.grosskopf@gmail.com>
2023-10-08 18:08:26 -0300

    Resolved hash inversion exercice

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -19,34 +19,38 @@ def most_frequent_bigram(str)
 end
 
 
+# Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
 class Hash
-    # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
-    def inverse
-
+  def inverse
+    hash = {}
+    self.each do |k, v|
+      hash[v] = k
     end
+    hash
+  end
 end
 
 
 class Array
-    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
-    def pair_sum_count(num)
+  # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
+  def pair_sum_count(num)
 
-    end
+  end
 
-    # Write a method, Array#bubble_sort, that takes in an optional proc argument.
-    # When given a proc, the method should sort the array according to the proc.
-    # When no proc is given, the method should sort the array in increasing order.
-    #
-    # Sorting algorithms like bubble_sort, commonly accept a block. That block accepts
-    # two parameters, which represents the two elements in the array being compared.
-    # If the block returns 1, it means that the second element passed to the block
-    # should go before the first (i.e. switch the elements). If the block returns -1,
-    # it means the first element passed to the block should go before the second
-    # (i.e. do not switch them). If the block returns 0 it implies that
-    # it does not matter which element goes first (i.e. do nothing).
-    #
-    # This should remind you of the spaceship operator! Convenient :)
-    def bubble_sort(&prc)
+  # Write a method, Array#bubble_sort, that takes in an optional proc argument.
+  # When given a proc, the method should sort the array according to the proc.
+  # When no proc is given, the method should sort the array in increasing order.
+  #
+  # Sorting algorithms like bubble_sort, commonly accept a block. That block accepts
+  # two parameters, which represents the two elements in the array being compared.
+  # If the block returns 1, it means that the second element passed to the block
+  # should go before the first (i.e. switch the elements). If the block returns -1,
+  # it means the first element passed to the block should go before the second
+  # (i.e. do not switch them). If the block returns 0 it implies that
+  # it does not matter which element goes first (i.e. do nothing).
+  #
+  # This should remind you of the spaceship operator! Convenient :)
+  def bubble_sort(&prc)
 
-    end
+  end
 end
