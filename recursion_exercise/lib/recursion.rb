2dcebfe9d56b7856fa6509c0cc708b3e6ace1300
vitor <vitor.grosskopf@gmail.com>
2023-10-15 16:38:58 -0300

    Finish recursion exercices

--- recursion_exercise/lib/recursion.rb
+++ recursion_exercise/lib/recursion.rb
@@ -13,3 +13,28 @@ def add_numbers(nums_array)
   nums_array[0] + add_numbers(nums_array[1..-1])
 
 end
+
+# Let's write a method that will solve Gamma Function recursively. The Gamma Function is defined Γ(n) = (n-1)!.
+
+def gamma(n)
+  return nil if n == 0
+  return 1 if n == 1
+  return (n-1) * gamma(n-1)
+end
+
+=begin
+  Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available at the ice cream shop, as well as the user's favorite ice cream flavor. Recursively find out whether or not the shop offers their favorite flavor.
+=end
+
+def ice_cream_shop(flavors, favorite)
+  return false if flavors.empty?
+  return true if flavors[0] == favorite
+  return ice_cream_shop(flavors[1..-1], favorite)
+end
+
+# Write a function reverse(string) that takes in a string and returns it reversed.
+def reverse(str)
+  return str if str.length <= 1
+
+  str[-1] + reverse(str[0..-2])
+end
