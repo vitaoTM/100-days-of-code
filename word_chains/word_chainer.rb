c306f844bb4e1575d06781f2764dc7fbf12fc7e6
vitor <vitor.grosskopf@gmail.com>
2023-10-17 20:44:57 -0300

    Not understanding what I am doing

--- word_chains/word_chainer.rb
+++ word_chains/word_chainer.rb
@@ -4,12 +4,12 @@ class WordChainer
 
   def initialize(dicctionary_file_name)
     @dictionay = read_dictionary(file_name)
-    @dictionay
+    @dictionay = Set.new(@dictionay)
   end
 
   def read_dictionary(file_name)
     words = []
-    File.open(words. 'r') do |file|
+    File.open(words, 'r') do |file|
       file.each_line do |line|
         words << line.chomp
       end
@@ -18,7 +18,36 @@ class WordChainer
   end
 
   def adjacent_words(word)
-    adjacent = []
+    adjacent_words = []
+
+    word.each_char.with_index do |old_letter, idx|
+      ('a'..'z').each do |new_letter|
+        next if old_letter == new_letter
+
+        new_word = word.dup
+        new_word[idx] = new_letter
+
+        adjacent_words << new_word if dictionary.include?(new_word)
+      end
+    end
+    adjacent_words
+  end
+
+  def explore_current_words
+    new_current_words = []
+
+    @current_words.each do |current_word|
+      adjacent_words(current_word).each do |adjacent_word|
+        next if @all_seen_words.key?(adjacent_word)
+
+        new_current_words << adjacent_word
+        @all_seen_words[adjacent_word] = current_word
+      end
+    end
+    @current_words = new_current_words
+  end
+
+  def build_path
 
 
   end
