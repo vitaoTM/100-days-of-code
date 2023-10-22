d5e9815da8f6f77ef38d75e59e2c518d4665de4b
vitor <vitor.grosskopf@gmail.com>
2023-10-22 00:55:25 -0300

    New project, fixing simon game

---
+++ simon/skeleton/lib/simon.rb
@@ -0,0 +1,43 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
+  end
+
+  def play
+
+  end
+
+  def take_turn
+
+  end
+
+  def show_sequence
+
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+
+  end
+end
