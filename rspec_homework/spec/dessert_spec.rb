3c094d80e84df0164582fb1e01671eba666dd9b3
vitor <vitor.grosskopf@gmail.com>
2023-10-25 20:23:15 -0300

    Add #make_more spec test

--- rspec_homework/spec/dessert_spec.rb
+++ rspec_homework/spec/dessert_spec.rb
@@ -66,6 +66,11 @@ describe Dessert do
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    before { allow(chef).to receive(:bake) }
+    it "calls bake on the dessert's chef with the dessert passed in" do
+
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
   end
 end
