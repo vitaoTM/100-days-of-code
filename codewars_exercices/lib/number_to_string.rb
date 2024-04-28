3886176af56cba795269a23adab670080fb58601
vitor <vitor.grosskopf@gmail.com>
2024-04-27 23:46:04 -0300

    Solved Kata: Convert a Number to a String! - Done in: 2024-04-27

---
+++ codewars_exercices/lib/number_to_string.rb
@@ -0,0 +1,18 @@
+=begin
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+Examples (input --> output):
+
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+find original: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/ruby
+=end
+
+def number_to_string(num)
+  num.to_s
+  # Convert a Number to a String
+end
---
+++ codewars_exercices/spec/number_to_String_spec.rb
@@ -0,0 +1,7 @@
+require 'number_to_string'
+
+describe 'number to string' do
+  it 'should pass test' do
+    expect(number_to_string(67)).to eq('67')
+  end
+end
