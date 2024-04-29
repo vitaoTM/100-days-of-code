bc2a729995ffaa699e2c9f7c9233793ef4e12e09
vitor <vitor.grosskopf@gmail.com>
2024-04-29 14:34:02 -0300

    Solved Kata: Powers of 2 - Done in: 2024-04-29

---
+++ codewars_exercices/spec/powers_of_two_spec.rb
@@ -0,0 +1,9 @@
+require 'powers_of_two'
+
+describe 'powers of two' do
+  it '#power_of_two' do
+    expect(powers_of_two(0)).to eq([1])
+    expect(powers_of_two(1)).to eq([1, 2])
+    expect(powers_of_two(4)).to eq([1, 2, 4, 8, 16])
+  end
+end
