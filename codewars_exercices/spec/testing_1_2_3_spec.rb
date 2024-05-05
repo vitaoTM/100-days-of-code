1232dcc0066b37ceddc0871eb82403852890cad4
vitor <vitor.grosskopf@gmail.com>
2024-05-05 16:50:46 -0300

    Solved Kata: Testing 1 2 3 Done in: 2024-05-05

---
+++ codewars_exercices/spec/testing_1_2_3_spec.rb
@@ -0,0 +1,8 @@
+require 'testing_1_2_3'
+
+describe 'Simple test' do
+  it '#numbers' do
+  expect(number([])).to eq([])
+  expect(number(["a", "b", "c"])).to eq(["1: a", "2: b", "3: c"])
+  end
+end
