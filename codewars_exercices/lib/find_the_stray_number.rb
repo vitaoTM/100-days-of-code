b5dc2941d39ea72f1f4afe539f3398a436245256
vitor <vitor.grosskopf@gmail.com>
2024-05-08 15:10:38 -0300

    Added Kata exercice: Find the stray number

---
+++ codewars_exercices/lib/find_the_stray_number.rb
@@ -0,0 +1,19 @@
+=begin
+  You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+Examples
+
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+
+find original: https://www.codewars.com/kata/57f609022f4d534f05000024/train/ruby
+
+=end
+
+def stray (numbers)
+  numbers.sort!
+  numbers.count(numbers[0]) > 1 ? numbers[-1] : numbers[0]
+end
