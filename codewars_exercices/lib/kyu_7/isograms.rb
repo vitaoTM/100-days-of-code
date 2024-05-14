96e457592df5f548fcaf0dbfdb8daef16c175809
vitor <vitor.grosskopf@gmail.com>
2024-05-14 17:09:43 -0300

    Add Kata: Isograms - Add spec file

---
+++ codewars_exercices/lib/kyu_7/isograms.rb
@@ -0,0 +1,12 @@
+=begin
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case)
+
+find original: https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/ruby
+
+=end
---
+++ codewars_exercices/spec/kyu_7/isograms_spec.rb
@@ -0,0 +1,8 @@
+require "isograms"
+
+describe "Simple Test" do
+  it '#isogram' do
+
+  end
+
+end
