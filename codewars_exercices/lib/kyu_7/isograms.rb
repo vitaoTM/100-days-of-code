c9411d30b4253bad19d8227c14be2303ecb68706
vitor <vitor.grosskopf@gmail.com>
2024-05-14 17:14:48 -0300

    Solved Kata: Isograms - Done in: 2024-05-14 - Difficulty: kyu 7 - Adds tests

--- codewars_exercices/lib/kyu_7/isograms.rb
+++ codewars_exercices/lib/kyu_7/isograms.rb
@@ -10,3 +10,10 @@ Example: (Input --> Output)
 find original: https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/ruby
 
 =end
+
+def isogram(string)
+  string.downcase.chars.tally.each do |_, v|
+    return false if v > 1
+  end
+  true
+end
--- codewars_exercices/spec/kyu_7/isograms_spec.rb
+++ codewars_exercices/spec/kyu_7/isograms_spec.rb
@@ -1,8 +1,13 @@
-require "isograms"
+require "kyu_7/isograms"
 
 describe "Simple Test" do
   it '#isogram' do
-
+    expect(isogram("Dermatoglyphics")).to eq( true )
+    expect(isogram("isogram")).to eq( true )
+    expect(isogram("aba")).to eq( false )
+    expect(isogram("moOse")).to eq( false)
+    expect(isogram("isIsogram")).to eq( false )
+    expect(isogram("")).to eq( true )
   end
 
 end
