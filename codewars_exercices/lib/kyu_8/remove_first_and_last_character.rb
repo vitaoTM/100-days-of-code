a66b056fe54adab5c07d42307509d9534853133e
vitor <vitor.grosskopf@gmail.com>
2024-05-17 16:25:32 -0300

    Added Kata: Remove First And Last Character - Added spec tests

---
+++ codewars_exercices/lib/kyu_8/remove_first_and_last_character.rb
@@ -0,0 +1,10 @@
+=begin
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
+find original: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/ruby
+
+=end
+
+def remove_char(s)
+
+end
---
+++ codewars_exercices/spec/kyu_8/remove_first_and_last_character_spec.rb
@@ -0,0 +1,11 @@
+require "kyu_8/remove_first_and_last_character"
+
+describe 'Simple Test' do
+  it '#remove_char' do
+    expect(remove_char('eloquent')).to eq('loquen')
+    expect(remove_char('country')).to eq('ountr')
+    expect(remove_char('person')).to eq('erso')
+    expect(remove_char('place')).to eq('lac')
+    expect(remove_char('ok')).to eq('')
+  end
+end
