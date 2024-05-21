49d864a6489182c357d8dc66adf77684bba423a0
vitor <vitor.grosskopf@gmail.com>
2024-05-21 19:08:57 -0300

    Solved Kata: Basic Mathematics Operations - Done in: 2024-05-21 - Difficulty: kyu 8 -- Added spec tests

---
+++ codewars_exercices/lib/kyu_8/basic_math_operation.rb
@@ -0,0 +1,28 @@
+=begin
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), v1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+Examples(Operator, value1, value2) --> output
+
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+
+find origianl:
+
+=end
+
+def basic_opp(operator, v1, v2)
+  case operator
+    when '+'
+       v1 + v2
+    when  '-'
+       v1 - v2
+    when  '*'
+       v1 * v2
+    when  '/'
+       v1 / v2
+  end
+end
---
+++ codewars_exercices/spec/kyu_8/basic_math_operation_spec.rb
@@ -0,0 +1,10 @@
+require 'kyu_8/basic_math_operation'
+
+describe "Simple tests" do
+  it "#basic_opp" do
+    expect(basic_opp('+', 4, 7)).to eq(11)
+    expect(basic_opp('-', 15, 18)).to eq(-3)
+    expect(basic_opp('*', 5, 5)).to eq(25)
+    expect(basic_opp('/', 49, 7)).to eq(7)
+  end
+end
