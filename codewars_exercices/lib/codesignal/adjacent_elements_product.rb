0277f4539bbc8d705ab4a116700f7fa4dcd5bf94
vitor <vitor.grosskopf@gmail.com>
2024-05-27 23:49:31 -0300

    Solved Codesignal exercice: Adjacent Elements Product - Done in: 2024-05-27 - Difficulty Edge of the Ocean - Adds spec tests

---
+++ codewars_exercices/lib/codesignal/adjacent_elements_product.rb
@@ -0,0 +1,40 @@
+=begin
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+solution(inputArray) = 21.
+
+7 and 3 produce the largest product.
+
+Input/Output
+
+[execution time limit] 4 seconds (rb)
+
+[memory limit] 1 GB
+
+[input] array.integer inputArray
+
+An array of integers containing at least two elements.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 10,
+-1000 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The largest product of adjacent elements.
+
+find original: https://app.codesignal.com/arcade/intro/level-2/xzKiBHjhoinnpdh6m
+
+=end
+
+def solution(array)
+  result = []
+  array.each_with_index do |e, idx|
+    break if array[idx + 1].nil?
+    result << array[idx + 1] * e
+  end
+  result.max
+end
