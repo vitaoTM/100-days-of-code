285b114c3ec71d475652d257534b9e143c3840e4
vitor <vitor.grosskopf@gmail.com>
2024-05-28 13:14:59 -0300

    Update Image link from codesignal exercice

--- codewars_exercices/lib/codesignal/shape_area.rb
+++ codewars_exercices/lib/codesignal/shape_area.rb
@@ -4,7 +4,7 @@ Below we will define an n-interesting polygon. Your task is to find the area of 
 A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
 
 ```
-  [image](/home/vitaotm/Documents/CodeWars/100-days-of-code/codewars_exercices/lib/codesignal/shape_area.png}
+  [image](https://codesignal.s3.amazonaws.com/uploads/1664318501/area.png?raw=true)
 
 ```
 =end
