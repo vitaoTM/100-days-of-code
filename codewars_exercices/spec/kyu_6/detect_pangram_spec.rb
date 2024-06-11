87930d0b01c6f07f82872203aa43ed6ee9407386
vitor <vitor.grosskopf@gmail.com>
2024-06-11 18:31:22 -0300

    Solved Kata: Detect Pangram - Done in: 2024-06-11 - Difficulty: kyu 6 - Added spec tests

---
+++ codewars_exercices/spec/kyu_6/detect_pangram_spec.rb
@@ -0,0 +1,10 @@
+require 'kyu_6/detect_pangram'
+
+describe "Pangram" do
+  it "Check if string is a Pangram" do
+    # "The quick brown fox jumps over the lazy dog."
+    expect(pangram?("The quick brown fox jumps over the lazy dog.")).to eq(true)
+    # "This is not a pangram."
+    expect(pangram?("This is not a pangram.")).to eq(false)
+  end
+end
