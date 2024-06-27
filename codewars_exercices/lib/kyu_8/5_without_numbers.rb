42bfd921ff8dd4076e043c679e1a391d21dc9822
vitor <vitor.grosskopf@gmail.com>
2024-06-26 22:11:43 -0300

    Solvee Kata: 5 without numbers - Done in: 2024-06-26 - Difficulty: kyu 8

---
+++ codewars_exercices/lib/kyu_8/5_without_numbers.rb
@@ -0,0 +1,14 @@
+=begin
+  Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+find original: https://www.codewars.com/kata/59441520102eaa25260000bf/train/ruby
+
+=end
+
+def unusual_five
+  'aaaaa'.size
+end
---
+++ codewars_exercices/spec/kyu_8/5_without_numbers_spec.rb
@@ -0,0 +1,21 @@
+require 'kyu_8/5_without_numbers'
+
+describe 'simple tests' do
+
+  # disallowed_chars = '0123456789*/+'
+  # it 'Should not use forbbiden characters' do
+  #   disallowed_chars = '0123456789*/+'
+  #   function_body = unusual_five.method(:body).source_location.source
+
+  #   expect(function_body).not_to match(/#{disallowed_chars}/)
+  #   # expect(always_five).to eq(5)
+  # end
+
+
+  it '#unusual_five' do
+    return 'error' if unusual_five.parse
+    expect(unusual_five).to eq(5)
+  end
+
+
+end
