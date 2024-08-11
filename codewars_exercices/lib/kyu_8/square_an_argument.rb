f3250432e7532bfbca3f4e46ac12c53f49f61cef
vitor <vitor.grosskopf@gmail.com>
2024-08-10 22:54:18 -0300

    - Solved Kata: Square an Argument
    - Done in: 2024-08-10
    - Difficulty: kyu 8
    - Adds 1 test case

---
+++ codewars_exercices/lib/kyu_8/square_an_argument.rb
@@ -0,0 +1,11 @@
+=begin
+
+write a function that takes an argument and returns the square of it.
+
+find origianl: https://www.codewars.com/kata/523b623152af8a30c6000027/train/ruby
+
+=end
+
+def square(n)
+  n ** 2
+end
---
+++ codewars_exercices/spec/kyu_8/square_an_argument_spec.rb
@@ -0,0 +1,8 @@
+require 'kyu_8/square_an_argument'
+
+describe 'Simple test' do
+  it '#square' do
+    expect(square(3)).to eq(9)
+  end
+
+end
