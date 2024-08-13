fe75737e1689bb61b92aa5ff6fab71e29f99e563
vitor <vitor.grosskopf@gmail.com>
2024-08-13 18:37:59 -0300

    Adds Testcases for Codesignal Exercice: Even Digits Only

--- codewars_exercices/spec/codesignal/even_digits_only_spec.rb
+++ codewars_exercices/spec/codesignal/even_digits_only_spec.rb
@@ -3,7 +3,9 @@ require 'codesignal/even_digits_only'
 describe 'Simple tests' do
   it '#even_digits' do
     expect(even_digits(248622)).to eq(true)
-
+    expect(even_digits(123)).to eq(false)
+    expect(even_digits(9878888)).to eq(false)
+    expect(even_digits(266864268)).to eq(true)
 
   end
 
