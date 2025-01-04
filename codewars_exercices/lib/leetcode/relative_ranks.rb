2ae924b5a9e3f4d17e6de309aacc6d1783b23b4f
vitor <vitor.grosskopf@gmail.com>
2025-01-03 21:13:50 -0300

    Solved Leetcode Exercice: Relative Ranks
    - Done in: 2025-01-03
    - Difficulty: Easy
    - Problem no.: 506
    - Day: 255

---
+++ codewars_exercices/lib/leetcode/relative_ranks.rb
@@ -0,0 +1,50 @@
+=begin
+  You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+The 1st place athlete's rank is "Gold Medal".
+The 2nd place athlete's rank is "Silver Medal".
+The 3rd place athlete's rank is "Bronze Medal".
+For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+
+
+Example 1:
+
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+Example 2:
+
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+
+find original: https://leetcode.com/problems/relative-ranks/description/
+
+=end
+
+def find_relative_number(score)
+  size = score.size
+  arr = Array.new(size)
+  top_3 = score.max(3)
+
+  arr[score.index(top_3[0])] = "Gold Medal"
+  arr[score.index(top_3[1])] = "Silver Medal" if size > 1
+  arr[score.index(top_3[2])] = "Bronze Medal" if size > 2
+
+  count = 4
+  dup = score.dup - top_3
+  size -= 3
+
+  while size > 0
+    arr[score.index(dup.max)] = "#{count}"
+    dup.delete(dup.max)
+    count += 1
+    size -= 1
+  end
+  arr
+end
