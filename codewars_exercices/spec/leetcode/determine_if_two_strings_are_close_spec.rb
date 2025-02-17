e9f31e7c91c6daa71bb34fcb299c0e4022b99468
vitor <vitor.grosskopf@gmail.com>
2025-02-17 13:28:13 -0300

    Solved Leetcode Exercice: Determine if Two Strings are Close
    - Done in: 2025-02-17
    - Problem no.: 1657
    - Difficulty: Medium
    - Day: 297

---
+++ codewars_exercices/spec/leetcode/determine_if_two_strings_are_close_spec.rb
@@ -0,0 +1,11 @@
+require 'leetcode/determine_if_two_strings_are_close'
+
+describe 'Simple test' do
+  it '#close_strings' do
+    expect(close_strings('abc', 'bca')).to eq(true)
+    expect(close_strings('a', 'aa')).to eq(false)
+    expect(close_strings('cabbba', 'abbccc')).to eq(true)
+    expect(close_strings('uau', 'ssx')).to eq(false)
+  end
+
+end
